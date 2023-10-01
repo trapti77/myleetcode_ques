8d627ab8e013931e29cb2739c0bf74a20f8078d2
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-10-01 21:43:15 +0530

    Sum multiples

---
+++ 22ques.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum=0;
+        while(n>=1)
+        {
+        
+            if(n%3==0||n%5==0||n%7==0)
+            {
+              sum+=n;
+            }
+            n--;
+        }
+        return sum;
+    }
+};
