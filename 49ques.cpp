43be75803f7d7b8355a6ad9ae0c35f814dc42e94
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-10-02 19:27:58 +0530

    binary tree level order traversal

---
+++ 49ques.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        if(root==NULL)
+        {
+            return ans;
+        }
+        q.push(root);
+        while(!q.empty())
+        {
+            vector<int>v;
+            int s=q.size();
+            if(s==0)
+            {
+                return ans;
+            }
+            while(s>0)
+            {
+            TreeNode*temp=q.front();
+            q.pop();
+            v.push_back(temp->val);
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+            s--;
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
