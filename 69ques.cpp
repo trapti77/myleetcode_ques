1b5f3c3c1c4cd0e94f17aac110264d9320173c58
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-10-03 19:33:49 +0530

    single string

---
+++ 69ques.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==1)
+        {
+            return nums[0];
+        }
+        int num=0;
+     map<int,int>m;
+     for(auto i:nums)
+     {
+         m[i]++;
+     }
+     for(auto e:m)
+     {
+         if(e.second==1)
+         num=e.first;
+     }
+    return num;  
+    }
+};
