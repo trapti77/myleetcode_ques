e545a1674fa052c1d3d61b33ef637761a58cf07d
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-10-03 19:45:19 +0530

    sort the array by parity

---
+++ 80ques.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n=nums.size();
+        if(n==2)
+        {
+            swap(nums[0],nums[1]);
+        }
+        
+       int c=0;
+    int c1=0;
+    vector<int>ans;
+    vector<int>aa;
+    for(int i=0;i<n;i++)
+    {
+        if(nums[i]%2==0)
+        {
+           ans.push_back(nums[i]);
+        }
+        else{
+          aa.push_back(nums[i]);  
+        }
+    }
+  for(int i=0;i<aa.size();i++)
+  {
+    ans.push_back(aa[i]);
+  }
+        return ans;
+    }
+};
