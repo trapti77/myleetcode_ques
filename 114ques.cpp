511be4bfa2c0e0e846f0634d95a2eb05235bfbd5
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-12-11 20:52:31 +0530

    Permutations

---
+++ 114ques.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int> > result;
+	    
+	    permuteRecursive(nums, 0, result);
+	    return result;
+    }
+    void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result){
+		if (begin >= num.size()) {
+		
+		    result.push_back(num);
+		    return;
+		}
+		
+		for (int i = begin; i < num.size(); i++) {
+		    swap(num[begin], num[i]);
+		    permuteRecursive(num, begin + 1, result);
+		
+		    swap(num[begin], num[i]);
+		}
+    }
+};
